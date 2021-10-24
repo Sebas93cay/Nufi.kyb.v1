eb897a066dec19b3e264c9f2834cda45ec9b3d9e
Seb Asus VS <sebas93cay@gmail.com>
2021-10-24 00:57:10 -0500

    Finish Sidebar and use npm packages instead of default ones
    Bundler parcel is used to compile the sass in folder assets,
    the compiled .js and .css is generated in wwwroot/dist/ but this
    dificults the use of url() for images option in css since the compiled
    images are stored in wwwroot/ instead.
    
    It's necesary to fix that mistake

---
+++ assets/bundle.js
@@ -0,0 +1,15 @@
+// Import styles
+import './scss/site.scss'
+import './scss/sidebar.scss'
+
+// Setup jquery
+import $ from 'jquery'
+window.$ = window.jQuery = $
+
+// Import other scripts
+import 'bootstrap'
+import 'jquery-validation'
+import 'jquery-validation-unobtrusive'
+
+import './js/site'
+import './js/sidebar'
