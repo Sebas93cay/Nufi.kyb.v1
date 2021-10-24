c784c7b3c1b91bbd7ed30460159e6bea8cb0aae2
Seb Asus VS <sebas93cay@gmail.com>
2021-10-24 16:56:39 -0500

    Create blank component Tabs.razor

---
+++ Components/HeaderBase.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Nufi.kyb.v1.Components
+{
+	public class HeaderBase : ComponentBase
+	{
+		[Parameter]
+		public string index { get; set; }
+	}
+}
