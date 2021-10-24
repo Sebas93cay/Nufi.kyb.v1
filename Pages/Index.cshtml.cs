0a6205e7e2a8f97b19c4844c16cde3cc5a204ccd
Seb Asus VS <sebas93cay@gmail.com>
2021-10-24 17:29:12 -0500

    Create Index page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,18 +8,34 @@ using Microsoft.Extensions.Logging;
 
 namespace Nufi.kyb.v1.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel: PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		public fieldForm[] fields { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
 
-        public void OnGet()
-        {
+		public class fieldForm
+		{
+			public fieldForm (string label, string id)
+			{
+				this.label = label;
+				this.id = id;
+			}
+			public string label { get; set; }
+			public string id { get; set; }
+		}
 
-        }
-    }
+		public void OnGet()
+		{
+			fields = new fieldForm[] {
+				new fieldForm("Razón Social*", "razonSocialField"),
+				new fieldForm("RFC", "RFCField"),
+				new fieldForm("Marca", "MarcaField")
+			};
+		}
+	}
 }
