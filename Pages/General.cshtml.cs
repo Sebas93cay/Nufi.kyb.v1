6d2398555dd6c777b2b889837069b096c63a747e
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-10-25 12:38:05 -0500

    Basic try using C# HTTPClient

--- Pages/General.cshtml.cs
+++ Pages/General.cshtml.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Nufi.kyb.v1.Pages
 {
     public class GeneralModel : PageModel
     {
-		public Seccion[] secciones;
+		public Seccion[] secciones { get; set; }
 		public Dato[] datosGenerales { get; set; }
 		public Dato[] datosDomiciliarios { get; set; }
-
 		public class Seccion
 		{
 			public Seccion (string titulo, Dato[] datos)
@@ -38,37 +38,45 @@ namespace Nufi.kyb.v1.Pages
 		{
 			return a + b;
 		}
-        public void OnGet()
+        public static async Task OnGet()
         {
-			datosGenerales = new Dato[] {
-				new Dato("Razón Social", "Texto Aquí"),
-				new Dato("Marca", "Texto Aquí"),
-				new Dato("Nacionalidad", "Texto Aquí"),
-				new Dato("Registro Federal de Contribuyentes (RFC)", "Texto Aquí"),
-				new Dato("Fecha de Constitución", "Texto Aquí"),
-				new Dato("Número de Escritura Constitutiva", "Texto Aquí"),
-				new Dato("Folio Mercantíl", "Texto Aquí"),
-				new Dato("Fecha de inscripción de PRP", "Texto Aquí"),
-				new Dato("Sector", "Texto Aquí"),
-				new Dato("Giro Mercantíl", "Texto Aquí"),
-				new Dato("Objeto Social", "Texto Aquí")
-			};
-			datosDomiciliarios = new Dato[] {
-				new Dato("Calle", "Texto Aquí"),
-				new Dato("Número Exterior", "Texto Aquí"),
-				new Dato("Número Interior", "Texto Aquí"),
-				new Dato("Entre Calles", "Texto Aquí"),
-				new Dato("Colonia", "Texto Aquí"),
-				new Dato("Código Postal", "Texto Aquí"),
-				new Dato("Entidad Federativa / Demarcación", "Texto Aquí"),
-				new Dato("País", "Texto Aquí"),
-				new Dato("Delegación / Municipio", "Texto Aquí"),
-			};
-			secciones = new Seccion[]
+			string url = "https://stoplight.io/mocks/alfredpianist/kyb-api/23013508/actas_constitutivas/consultar/";
+			using (var client = new HttpClient())
 			{
-				new Seccion("Información General", datosGenerales),
-				new Seccion("Información Domiciliaria", datosDomiciliarios)
-			};
+				var response = await client.GetAsync(url);
+				var responseString = await response.Content.ReadAsStringAsync();
+			}
+			// datosGenerales = new Dato[]
+			// {
+			// 	new Dato("Razón Social", "Texto Aquí"),
+			// 	new Dato("Marca", "Texto Aquí"),
+			// 	new Dato("Nacionalidad", "Texto Aquí"),
+			// 	new Dato("Registro Federal de Contribuyentes (RFC)", "Texto Aquí"),
+			// 	new Dato("Fecha de Constitución", "Texto Aquí"),
+			// 	new Dato("Número de Escritura Constitutiva", "Texto Aquí"),
+			// 	new Dato("Folio Mercantil", "Texto Aquí"),
+			// 	new Dato("Fecha de inscripción de PRP", "Texto Aquí"),
+			// 	new Dato("Sector", "Texto Aquí"),
+			// 	new Dato("Giro Mercantíl", "Texto Aquí"),
+			// 	new Dato("Objeto Social", "Texto Aquí")
+			// };
+			// datosDomiciliarios = new Dato[]
+			// {
+			// 	new Dato("Calle", "Texto Aquí"),
+			// 	new Dato("Número Exterior", "Texto Aquí"),
+			// 	new Dato("Número Interior", "Texto Aquí"),
+			// 	new Dato("Entre Calles", "Texto Aquí"),
+			// 	new Dato("Colonia", "Texto Aquí"),
+			// 	new Dato("Código Postal", "Texto Aquí"),
+			// 	new Dato("Entidad Federativa / Demarcación", "Texto Aquí"),
+			// 	new Dato("País", "Texto Aquí"),
+			// 	new Dato("Delegación / Municipio", "Texto Aquí"),
+			// };
+			// secciones = new Seccion[]
+			// {
+			// 	new Seccion("Información General", datosGenerales),
+			// 	new Seccion("Información Domiciliaria", datosDomiciliarios)
+			// };
         }
     }
 }
