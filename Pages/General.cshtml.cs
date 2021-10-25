19472e923f469cad302d7b39bf64efbd8159c7a3
Seb Asus VS <sebas93cay@gmail.com>
2021-10-24 19:29:53 -0500

    Start doing General view

--- Pages/General.cshtml.cs
+++ Pages/General.cshtml.cs
@@ -9,6 +9,10 @@ namespace Nufi.kyb.v1.Pages
 {
     public class GeneralModel : PageModel
     {
+		public string createName(string a, string b)
+		{
+			return a + b;
+		}
         public void OnGet()
         {
         }
