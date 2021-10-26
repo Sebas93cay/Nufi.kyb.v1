ec9a1797268e1c6754ce0795eaed0bc41dcb1674
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-10-25 20:45:53 -0500

    add name of current page to dropdown button

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,7 +1,23 @@
 const dropdownButton = document.querySelector("#dropdown");
 const dropdownItems = document.querySelectorAll(".dropdown-item");
+const currentPage = document.title.split(' ');
+const buttonName = [];
 
-console.log(dropdownItems);
+let withinSeparator = false;
+
+currentPage.forEach((word) => {
+  if (withinSeparator === true) {
+    buttonName.push(word);
+  }
+  if (word === '-') {
+    if (withinSeparator === true) {
+      buttonName.pop();
+    }
+    withinSeparator = withinSeparator ? false : true;
+  } 
+});
+
+dropdownButton.innerHTML = buttonName.join(' ');
 
 dropdownItems.forEach((dropdownItem) => {
   dropdownItem.addEventListener("click", () => {
