76998125707bfd24223e7e89365d28f4c5a8f0ce
unknown <joel.ramirez@correounivalle.edu.co>
2021-10-25 22:22:04 -0500

    fix models for SAT

--- Models/SATRequest.cs
+++ Models/SATRequest.cs
@@ -4,7 +4,6 @@ namespace Nufi.kyb.v1.Models
 	{
         public string status { get; set; }
         public string message { get; set; }
-        public int codigo_postal { get; set; }
         public SATData[] data { get; set; }
 	}
 }
